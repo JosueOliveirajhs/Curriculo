c2339603c3d7b800f8a4978e1d612867ff6c7467
JosueOliveirajhs <161727242+JosueOliveirajhs@users.noreply.github.com>
2025-04-02 10:28:37 -0300

    Delete Curriculo-main/Curriculo-main directory

--- Curriculo-main/Curriculo-main/curriculo/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".progress").forEach(progress => {
-        let value = progress.getAttribute("data-progress");
-        progress.style.width = value + "%";
-    });
-});
